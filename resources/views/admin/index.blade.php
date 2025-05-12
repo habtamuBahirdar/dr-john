989bbab4b1ba0967c792496d7c8c2338474e9962
habtamuBahirdar <habtamu.ayeneweth@gmail.com>
2025-05-12 15:37:13 +0300

    uniformed

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document sss</title>
+    <title>Document ssso</title>
 </head>
 <body>
     <h1>admin home</h1>
